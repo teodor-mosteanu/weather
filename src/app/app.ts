6afd35da17cfa97ab05c6b2c67af7e5623ea9863
Teodor Mosteanu <teodor.mosteanu@gmail.com>
2025-08-14 18:00:14 +0100

    removed unused import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { WeatherDisplay } from './pages/weather-display/weather-display';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WeatherDisplay],
+  imports: [WeatherDisplay],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
